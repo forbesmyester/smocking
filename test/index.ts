c1d69d55333f7fc5cc06ba2537e9f9f61a912266
Matthew Forrester <matthew.forrester@speechmarks.com>
2016-10-29 09:18:48 +0100

    test: Ensure that we can call the function multiple times.

--- test/index.ts
+++ test/index.ts
@@ -5,26 +5,34 @@ import test from 'ava';
 test('Can call a smocked function', function(t) {
 
     let s = smocking()
-        .add(({callIndex}) => { return callIndex == 0; })
+        .add(({callIndex}) => { return callIndex < 2; })
         .returns(42)
         .calls(3, "HELLO", [null, 'OK'])
+        .add(({callIndex}) => { return true })
+        .returns(43)
+        .calls(1, "GOOD MORNING", [null, 'WHO ARE YOU?'])
 
     let f = s.get();
-    let that: string|null = null;
-    let msg: string|null = null;
+    let that: string[] = [];
+    let msg: string[] = [];
+    let r: number[] = [];
 
     function next(this: string, err, myMsg) {
-        that = this;
-        msg = myMsg;
+        that.push(this);
+        msg.push(myMsg);
     }
 
-    let r = f(1, 2, 3, next);
+    r = [f(1, 2, 3, next), f(4, 5, 6, next), f(7, next)];
 
-    t.is(that, 'HELLO');
-    t.is(msg, 'OK');
-    t.is(r, 42);
+    t.deepEqual(that, ['HELLO', 'HELLO', 'GOOD MORNING']);
+    t.deepEqual(msg, ['OK', 'OK', "WHO ARE YOU?"]);
+    t.deepEqual(r, [42, 42, 43]);
     t.deepEqual(
-        [{ checkIndex: 0, params: [1, 2, 3, next], called: 3, returned: 42}],
+        [
+            { checkIndex: 0, params: [1, 2, 3, next], called: 3, returned: 42},
+            { checkIndex: 0, params: [4, 5, 6, next], called: 3, returned: 42},
+            { checkIndex: 1, params: [7, next], called: 1, returned: 43}
+        ],
         s.invocations
     );
 
